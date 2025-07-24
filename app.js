ca20eaa3c60685348c0c0f45de5ca6fd86c233e8
itsvikash93 <vikashkumar827070@gmail.com>
2025-07-24 22:11:41 +0530

    small changes

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
+const serverless = require("serverless-http"); // 🔥
+
 const path = require("path");
 const mongooseConnection = require("./config/mongoose");
 const expressSession = require("express-session");
 const methodOverride = require("method-override");
 const flash = require("connect-flash");
+
+require("dotenv").config();
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
@@ -12,11 +16,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
-require("dotenv").config();
-
 app.use(
   expressSession({
-    secret: process.env.EXPRESS_SESSION_SECRET,
+    secret: process.env.EXPRESS_SESSION_SECRET || "secret",
     resave: false,
     saveUninitialized: false,
     cookie: { maxAge: 1000 * 60 * 60 * 24 },
@@ -24,15 +26,18 @@ app.use(
 );
 app.use(flash());
 
+// Routers
 const indexRouter = require("./routes/index");
 const userRouter = require("./routes/user");
 const hisaabRouter = require("./routes/hisaab");
-const isLoggedIn = require("./middlewares/isLoggedIn");
-
-// HomePage
 
 app.use("/", indexRouter);
 app.use("/", userRouter);
 app.use("/hisaab", hisaabRouter);
 
-app.listen(process.env.PORT || 3000);
+// Don't use app.listen()
+// Export for Vercel
+module.exports = app;
+module.exports.handler = serverless(app);
+
+// app.listen(process.env.PORT || 3000);
